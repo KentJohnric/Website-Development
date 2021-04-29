50473bbcf9068d2fa15864b2c9a03ec263433ad5
Kent Johnric Sardina <Sardinakent08@gmail.com>
2021-04-29 16:02:17 +0800

    Login&SignUp
    Stable version of Login and SignUp

---
+++ HydroMolino/Version3(Fail)/1.js
@@ -0,0 +1,7 @@
+const sign_in_btn = document.querySelector('#sign-in-button');
+const sign_up_bth = document.querySelector('#sign-up-button');
+const container = document.querySelector('.container');
+
+sign_up_btn.addEventListener('click',()=>{container.classList.add('sign-up-mode')});
+
+sign_in_btn.addEventListener('click',()=>{container.classList.remove('sign-up-mode')});
